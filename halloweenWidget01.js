4621c1174e3174c03d98092037fe20f49f49008e
Hoji Palette Studio <161171919+HojiPaletteStudio@users.noreply.github.com>
2024-09-17 15:23:39 +0700

    Add files via upload

---
+++ halloweenWidget01.js
@@ -0,0 +1,143 @@
+export let follow = `
+<div class="follow-alert">
+    <div class="component-box-invisible bat-box">
+            <div class="loop-bat bat1">
+                  <video  autoplay loop width="60" src="https://cdn.jsdelivr.net/gh/HojiPaletteStudio/galleryVideo/red-one-bat-flying0001-0080.webm" type="video/webm">
+            </div>
+            <div class="loop-bat bat2">
+                  <video autoplay loop width="60" src="https://cdn.jsdelivr.net/gh/HojiPaletteStudio/galleryVideo/red-one-bat-flying0001-0080.webm" type="video/webm">
+            </div>
+              <div class="loop-bat bat3">
+                  <video autoplay loop width="90" src="https://cdn.jsdelivr.net/gh/HojiPaletteStudio/galleryVideo/red-one-bat-flying0001-0080.webm" type="video/webm">
+            </div>
+                <div class="loop-bat bat4">
+                  <video autoplay loop width="90" src="https://cdn.jsdelivr.net/gh/HojiPaletteStudio/galleryVideo/red-one-bat-flying0001-0080.webm" type="video/webm">
+            </div>
+     </div>
+    <div class="component-box">
+        <div class="candle-box">
+                 <svg
+                  width="143"
+                  height="200"
+                  viewBox="0 0 143 200"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g class="Group 63">
+                    <g class="Group 4">
+                      <g class="fire">
+                        <path
+                          d="M26.1273 83.032C32.5934 91.1073 41.255 93.5 41.255 93.5C41.255 93.5 55.4634 92.7024 60.4515 71C64.093 55.1565 47.9393 16.5 41.255 26C34.5706 35.5 19.6611 74.9567 26.1273 83.032Z"
+                          fill="#A6FFDF"
+                        />
+                        <path
+                          d="M26.1273 83.032C32.5934 91.1073 41.255 93.5 41.255 93.5C41.255 93.5 55.4634 92.7024 60.4515 71C64.093 55.1565 47.9393 16.5 41.255 26C34.5706 35.5 19.6611 74.9567 26.1273 83.032Z"
+                          stroke="#00FFA4"
+                          stroke-width="3"
+                        />
+                      </g>
+                      <path
+                        class="candle"
+                        d="M65.2441 155.684C66.8248 133.18 68.6592 97.0643 63.889 99.9833C56.9984 104.2 27.8 103.845 18.9049 93.7899C12.5233 86.5761 15.2983 130.482 17.3761 155.872C18.1725 165.603 26.3418 173 36.1056 173H46.5087C56.3477 173 64.5547 165.499 65.2441 155.684Z"
+                        fill="#7545FF"
+                      />
+                    </g>
+                    <g class="Group 19">
+                      <g class="fire">
+                        <path
+                          d="M98.8677 136.93C102.594 141.613 107.585 143 107.585 143C107.585 143 115.773 142.538 118.647 129.953C120.746 120.766 111.437 98.3509 107.585 103.86C103.733 109.368 95.1416 132.248 98.8677 136.93Z"
+                          fill="#A6FFDF"
+                        />
+                        <path
+                          d="M98.8677 136.93C102.594 141.613 107.585 143 107.585 143C107.585 143 115.773 142.538 118.647 129.953C120.746 120.766 111.437 98.3509 107.585 103.86C103.733 109.368 95.1416 132.248 98.8677 136.93Z"
+                          stroke="#00FFA4"
+                          stroke-width="3"
+                        />
+                      </g>
+                      <path
+                        class="candle"
+                        d="M91.4735 148.5C102.974 149.713 110.473 149.213 127.973 146C139.489 143.886 138.447 167.937 136.388 184.633C135.28 193.615 127.463 200 118.412 200H99.6164C89.2635 200 80.7994 191.706 81.3658 181.368C82.1899 166.33 84.597 147.775 91.4735 148.5Z"
+                        fill="#7545FF"
+                      />
+                    </g>
+                  </g>
+
+                </svg>
+
+        </div>
+
+        <div class="name">
+                <div class="cross-sign">
+                  <svg width="78" height="84" viewBox="0 0 78 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="29.9515" y="1.52588e-05" width="20" height="84" fill="#D6CEED"/>
+                  <rect x="0.951477" y="42" width="20" height="77" transform="rotate(-90 0.951477 42)" fill="#D6CEED"/>
+                  </svg>
+                </div>
+                <div class="name-text">
+                   name
+                 </div>
+        </div>
+
+        <div class="candle-box">
+
+
+                <svg width="143" height="200" viewBox="0 0 143 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g class="Group 47">
+                    <g class="Group 20">
+                    <g class="fire" filter="url(#filter0_d_50_7)">
+                    <path d="M117.507 82.5144C111.04 90.5897 102.379 92.9824 102.379 92.9824C102.379 92.9824 88.1704 92.1849 83.1823 70.4824C79.5408 54.6389 95.6945 15.9824 102.379 25.4824C109.063 34.9824 123.973 74.4391 117.507 82.5144Z" fill="#A6FFDF"/>
+                    <path d="M117.507 82.5144C111.04 90.5897 102.379 92.9824 102.379 92.9824C102.379 92.9824 88.1704 92.1849 83.1823 70.4824C79.5408 54.6389 95.6945 15.9824 102.379 25.4824C109.063 34.9824 123.973 74.4391 117.507 82.5144Z" stroke="#00FFA4" stroke-width="3"/>
+                    </g>
+                    <path class="candle" d="M78.3897 155.166C76.809 132.663 74.9746 96.5468 79.7448 99.4657C86.6354 103.682 115.834 103.328 124.729 93.2723C131.11 86.0585 128.336 129.964 126.258 155.354C125.461 165.085 117.292 172.482 107.528 172.482H97.1251C87.286 172.482 79.0791 164.981 78.3897 155.166Z" fill="#7545FF"/>
+                    </g>
+                    <g class="Group 21">
+                    <path class="candle" d="M52.1603 147.982C40.6603 149.196 33.1603 148.696 15.6603 145.482C4.14463 143.368 5.18644 167.419 7.24556 184.116C8.35329 193.098 16.1713 199.482 25.2213 199.482H44.0174C54.3703 199.482 62.8344 191.188 62.2679 180.851C61.4439 165.813 59.0368 147.257 52.1603 147.982Z" fill="#7545FF"/>
+                    <g class="fire" filter="url(#filter1_d_50_7)">
+                    <path d="M44.7661 136.412C41.0399 141.095 36.0486 142.482 36.0486 142.482C36.0486 142.482 27.8609 142.02 24.9865 129.436C22.8881 120.249 32.1967 97.8334 36.0486 103.342C39.9005 108.851 48.4922 131.73 44.7661 136.412Z" fill="#A6FFDF"/>
+                    <path d="M44.7661 136.412C41.0399 141.095 36.0486 142.482 36.0486 142.482C36.0486 142.482 27.8609 142.02 24.9865 129.436C22.8881 120.249 32.1967 97.8334 36.0486 103.342C39.9005 108.851 48.4922 131.73 44.7661 136.412Z" stroke="#00FFA4" stroke-width="3"/>
+                    </g>
+                    </g>
+                    </g>
+                    <defs>
+                    <filter id="filter0_d_50_7" x="58.7545" y="0.100061" width="84.242" height="116.794" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset/>
+                    <feGaussianBlur stdDeviation="11.2"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.647059 0 0 0 0 1 0 0 0 0 0.87451 0 0 0 1 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_50_7"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_50_7" result="shape"/>
+                    </filter>
+                    <filter id="filter1_d_50_7" x="0.782404" y="78.5825" width="68.7999" height="87.8115" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+                    <feOffset/>
+                    <feGaussianBlur stdDeviation="11.2"/>
+                    <feComposite in2="hardAlpha" operator="out"/>
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.647059 0 0 0 0 1 0 0 0 0 0.87451 0 0 0 1 0"/>
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_50_7"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_50_7" result="shape"/>
+                    </filter>
+                    </defs>
+                    </svg>
+
+        </div>
+    </div>
+        <div class="component-box-invisible">
+          <svg class="candle-liquid" width="829" height="168" viewBox="0 0 829 168" fill="none" xmlns="http://www.w3.org/2000/svg">
+
+                <path d="M113.032 10.6435C151.685 12.2713 181.505 10.6435 213.555 10.6435C245.606 10.6435 420.826 11.9628 482.842 11.9628C544.858 11.9628 564.255 18.561 614.19 13.4096C782.951 -3.99997 784.951 6.6769 805 19C825.049 31.3231 831.667 36.5 822 65.5C812.333 94.5 814.951 132.5 805 127.5C795.049 122.5 777.98 32.5 765.5 58C753.02 83.5 754.748 156.198 744.951 150C735.155 143.802 736.763 130.211 718 102.5C699.237 74.7887 692.81 95.6935 625.5 58C576.95 30.812 555 72.5 535.5 58C516 43.5 463.951 42 405 37C346.049 32 297 63 266.5 83.5C236 104 227.71 37.0232 185.451 37C129.633 36.9693 138.919 165.539 120.951 166C102.984 166.461 115.797 41.8407 78.4515 37C41.1059 32.1593 48.9515 116 33.227 116C17.5026 116 18.4515 92.4379 6.95148 58C4.28338 50.0101 -14.8095 33.9164 33.227 10.6435C81.2636 -12.6295 81.6561 9.32207 113.032 10.6435Z" fill="#7545FF"/>
+
+
+        </svg>
+
+        </div>
+
+        <div  class="text-mess">
+       Just Followed
+          </div>
+
+
+</div>
+`;
